05cb78d7a3e354a0864a1b72ac66fbb6f496f5e7
Nofre <davidprogram417@gmail.com>
2024-12-19 22:02:44 +0100

    user-service: added create user function

--- internal/users-service/repository/reposotiry.go
+++ internal/users-service/repository/reposotiry.go
@@ -35,7 +35,6 @@ func (r *Repository) GetUserByID(ctx context.Context, userId int) (*User, error)
 		Where(squirrel.Eq{"deleted_at": nil})
 
 	query, args, err := builder.Build()
-
 	if err != nil {
 		return nil, err
 	}
@@ -61,4 +60,43 @@ func (r *Repository) GetUserByID(ctx context.Context, userId int) (*User, error)
 	}
 
 	return &user, nil
+}
+
+func (r *Repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
+	return nil, nil
+}
+
+func (r *Repository) CreateUser(ctx context.Context, user *UserWithPassword) (*User, error) {
+	builder := dbx.NewStatementBuilder()
+
+	builder.Insert("users").
+		Columns("email", "password_hash", "avatar_url", "username", "full_name", "bio").
+		Values(user.Email, user.PasswordHash, user.AvatarURL, user.Username, user.FullName, user.Bio).
+		Suffix("RETURNING id, role, created_at")
+
+	query, args, err := builder.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.db.QueryRow(ctx, query, args...).Scan(&user.ID, &user.Role, &user.CreatedAt)
+
+	switch {
+	case dbx.IsUniqueViolation(err, "email"):
+		return nil, apperrors.AlreadyExists("user", "email", user.Email)
+	case dbx.IsUniqueViolation(err, "username"):
+		return nil, apperrors.AlreadyExists("user", "username", user.Username)
+	case err != nil:
+		return nil, apperrors.Internal(err)
+	}
+
+	return user.User, nil
+}
+
+func (r *Repository) UpdateUser(ctx context.Context, user *User) error {
+	return nil
+}
+
+func (r *Repository) DeleteUser(ctx context.Context, userId int) error {
+	return nil
 }
--- internal/users-service/service/service.go
+++ internal/users-service/service/service.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"golang.org/x/crypto/bcrypt"
+	apperrors "story-pulse/internal/shared/error"
 	. "story-pulse/internal/users-service/models"
 	. "story-pulse/internal/users-service/repository"
 )
@@ -16,4 +18,13 @@ func NewService(repo *Repository) *Service {
 
 func (s *Service) GetUserByID(ctx context.Context, userId int) (*User, error) {
 	return s.repo.GetUserByID(ctx, userId)
+}
+
+func (s *Service) CreateUser(ctx context.Context, user *CreateUserRequest) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, apperrors.InternalWithoutStackTrace(err)
+	}
+
+	return s.repo.CreateUser(ctx, user.ToUserWithPassword(string(hash)))
 }
